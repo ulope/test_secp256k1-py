b9bc61809f6d3443bff0223c1313fa8f0903f2c7
Ulrich Petri <ulo@ulo.pe>
2016-07-15 19:34:58 +0200

    Travis autodeploy test XXXIV

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,23 +6,17 @@ echo "deploy"
 
 # remove left over files from previous stages
 rm -rf build
+mkdir build
 
-mkdir build wheelhouse
-
-env
+python setup.py sdist
 
 # On linux we want to build `manylinux1` wheels. See:
 if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
 	docker run --rm -v $(pwd):/io ${WHEELBUILDER_IMAGE} /io/.travis/build-linux-wheels.sh
 else
-	python -m pip install wheel
-
-	if [[ "${TRAVIS_PYTHON_VERSION}" == "2.7" && "$TRAVIS_OS_NAME" == "linux" ]]; then
-		python setup.py sdist
-	fi
-
 	# Only build wheels for the non experimental bundled version
 	if [[ ${BUNDLED} -eq 1 && ${SECP_BUNDLED_EXPERIMENTAL} -eq 0 && "$TRAVIS_OS_NAME" == "osx" ]]; then
+		python -m pip install wheel
 		python setup.py bdist_wheel
 	fi
 fi
@@ -42,11 +36,7 @@ EOF
 
 python -m pip install twine
 
-twine register --repository pypitest secp256k1
-
-# Ignore globs for non existing files
-shopt -s nullglob
-
+twine register --repository pypitest dist/secp256k1*.tar.gz
 twine upload --repository pypitest --skip-existing dist/secp256k1*.{whl,gz}
 
 set +e +x
